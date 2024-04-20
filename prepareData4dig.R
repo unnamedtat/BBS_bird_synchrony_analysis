576274ca37f19799a9332d9d1d7aed6b864ae80f
unnamedtat <2641167724@qq.com>
2024-04-20 18:18:45 +0800

    add test function, but failed

---
+++ prepareData4dig.R
@@ -0,0 +1,17 @@
+load("NUSABird/2023Release_Nor/Workflow_total/1966-2022/filtered_itp_list.RData")
+
+library(openxlsx)
+library(purrr)
+
+# 没有则创建目录
+if (!dir.exists("NUSABird/2023Release_Nor/Workflow_total/1966-2022/filtered_itp_list")) {
+  dir.create("NUSABird/2023Release_Nor/Workflow_total/1966-2022/filtered_itp_list")
+}
+purrr::map2(names(filtered_itp_list),filtered_itp_list,function(AOU,x){
+  x_names<-as.data.frame(names(x))
+  y<-t(as.data.frame(x))%>%
+    cbind(x_names)
+  y$`names(x)`<-as.integer( y$`names(x)`)
+
+    write.xlsx(y, file = paste0("NUSABird/2023Release_Nor/Workflow_total/1966-2022/filtered_itp_list/",AOU,".xlsx"))
+})
