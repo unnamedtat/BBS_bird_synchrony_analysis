10dcaab7a4524fdea3ec1d76c72db0179cf5248f
unnamedtat <2641167724@qq.com>
2024-04-27 11:22:09 +0800

    file name

--- dig/prepareData4dig.R
+++ dig/prepareData4dig.R
@@ -1,11 +1,14 @@
-load("NUSABird/2023Release_Nor/Workflow_total/1971-2022/filtered_itp_list.RData")
+data_path <- "NUSABird/2023Release_Nor/Workflow_total/1971-2022"
+
+load(file.path(data_path, "filtered_itp_list.RData"))
 
 library(openxlsx)
 library(purrr)
 
+prepared_data_path <- file.path(data_path, "filtered_itp_list")
 # 没有则创建目录
-if (!dir.exists("NUSABird/2023Release_Nor/Workflow_total/1971-2022/filtered_itp_list")) {
-  dir.create("NUSABird/2023Release_Nor/Workflow_total/1971-2022/filtered_itp_list")
+if (!dir.exists(prepared_data_path) ){
+  dir.create(prepared_data_path)
 }
 purrr::map2(names(filtered_itp_list),filtered_itp_list,function(AOU,x){
   x_names<-as.data.frame(names(x))
@@ -16,5 +19,5 @@ purrr::map2(names(filtered_itp_list),filtered_itp_list,function(AOU,x){
 
   y$`names(x)`<-as.integer( y$`names(x)`)
 
-    openxlsx::write.xlsx(y, file = paste0("NUSABird/2023Release_Nor/Workflow_total/1971-2022/filtered_itp_list/",AOU,".xlsx"))
+    openxlsx::write.xlsx(y, file = file.path(prepared_data_path,paste0(AOU,".xlsx")))
 })
