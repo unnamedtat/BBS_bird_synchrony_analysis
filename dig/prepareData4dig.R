9db6a79219afcc429b257f8564dc04e7260b9397
unnamedtat <2641167724@qq.com>
2024-05-02 20:27:55 +0800

    add function

--- dig/prepareData4dig.R
+++ dig/prepareData4dig.R
@@ -1,23 +1,28 @@
-data_path <- "NUSABird/2023Release_Nor/Workflow_window/1971-1988"
-
-load(file.path(data_path, "filtered_itp_list.RData"))
-
 library(openxlsx)
 library(purrr)
 
-prepared_data_path <- file.path(data_path, "filtered_itp_list")
-# 没有则创建目录
-if (!dir.exists(prepared_data_path) ){
-  dir.create(prepared_data_path)
+workflow_dir <- "NUSABird/2023Release_Nor/Workflow_window"
+sub_dirs <- list.dirs(workflow_dir, recursive = TRUE, full.names = TRUE)
+
+for(data_path in sub_dirs){
+  if (file.exists(file.path(data_path, "filtered_itp_list.RData"))){
+    load(file.path(data_path, "filtered_itp_list.RData"))
+    prepared_data_path <- file.path(data_path, "filtered_itp_list")
+    # 没有则创建目录
+    if (!dir.exists(prepared_data_path) ){
+      dir.create(prepared_data_path)
+    }
+    purrr::map2(names(filtered_itp_list),filtered_itp_list,function(AOU,x){
+      x_names<-as.data.frame(names(x))
+
+      y<-t(as.data.frame(x))%>%
+        round(0)%>%
+        cbind(x_names)
+
+      y$`names(x)`<-as.integer( y$`names(x)`)
+
+        openxlsx::write.xlsx(y, file = file.path(prepared_data_path,paste0(AOU,".xlsx")))
+    })
+  }
 }
-purrr::map2(names(filtered_itp_list),filtered_itp_list,function(AOU,x){
-  x_names<-as.data.frame(names(x))
 
-  y<-t(as.data.frame(x))%>%
-    round(0)%>%
-    cbind(x_names)
-
-  y$`names(x)`<-as.integer( y$`names(x)`)
-
-    openxlsx::write.xlsx(y, file = file.path(prepared_data_path,paste0(AOU,".xlsx")))
-})
